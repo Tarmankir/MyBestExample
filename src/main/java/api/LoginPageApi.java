fee6eee4e3a8953930b57171f99956c208e9e1ec
aleksey_krapivnitskiy <krapivnitskiy@secureducation.ru>
2021-04-19 19:39:03 +0300

    added todos

--- src/main/java/api/LoginPageApi.java
+++ src/main/java/api/LoginPageApi.java
@@ -1,13 +1,30 @@
 package api;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.http.ContentType.TEXT;
+import static org.apache.http.HttpStatus.SC_OK;
 
 public class LoginPageApi {
+
+    //GPATH
+
+    //todo add new requests
+    //todo assert response with .extract().response().jsonPath()
+    //todo assert json schema with matchJsonSchema
+
 
     private static String requestBody = "{\n" +
             "  \"email\": \"umk090099888@yandex.ru\",\n" +
@@ -21,15 +38,14 @@ public class LoginPageApi {
 
     @Test
     public void postRequest() {
-        Response response = given()
-                .header("Content-type", "application/json")
-                .and()
+        given()
+                .contentType(JSON)
                 .body(requestBody)
-                .when()
-                .post("/posts")
-                .then()
+        .when()
+                .post("http://users.bugred.ru/tasks/rest/doregister" + "/posts")
+        .then()
+                .statusCode(SC_OK)
+                .contentType(JSON)
                 .extract().response();
-
-        Assertions.assertEquals(201, response.statusCode());
     }
 }
