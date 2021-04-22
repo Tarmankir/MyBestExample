eab87de150d68d0f5adaf8653ca77aecf78c1db9
k.nosov <k.nosov@rabota.ru>
2021-04-22 18:36:15 +0300

    release num 15

--- src/main/java/common/options/CucumberRunner.java
+++ src/main/java/common/options/CucumberRunner.java
@@ -3,6 +3,6 @@ package common.options;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "featureFiles", glue = "pages")
+@CucumberOptions(features = "test.resources.featureFiles", glue = "pages")
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 }
