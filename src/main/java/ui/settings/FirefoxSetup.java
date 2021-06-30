2b5aa6afb86abe3eb0bf3b3a02570d6f14c68630
k.nosov <k.nosov@rabota.ru>
2021-06-30 15:06:54 +0300

    gpu  for jenkins

--- src/main/java/ui/settings/FirefoxSetup.java
+++ src/main/java/ui/settings/FirefoxSetup.java
@@ -9,7 +9,7 @@ public abstract class FirefoxSetup {
 
     public FirefoxOptions getOptions() {
         options = new FirefoxOptions();
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("window-size=1280x768");
         FirefoxDriverManager.firefoxdriver().setup();
