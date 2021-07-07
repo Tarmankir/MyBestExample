787e3e89b6e32e139e2fc10c0865b070592464fb
k.nosov <k.nosov@rabota.ru>
2021-07-07 14:35:23 +0300

    verb

--- src/main/java/ui/settings/DriverSetup.java
+++ src/main/java/ui/settings/DriverSetup.java
@@ -9,7 +9,8 @@ public abstract class DriverSetup {
 
     public ChromeOptions getOptions() {
         options = new ChromeOptions();
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
+        options.addArguments("--verbose");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("window-size=1280,768");
