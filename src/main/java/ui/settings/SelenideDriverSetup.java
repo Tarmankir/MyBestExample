416f0c75f52c5323f214abf75ddfe78b6f992b09
Tarmankir <bigkir@yandex.ru>
2021-07-14 12:58:52 +0300

    Fix selenide driver setup

--- src/main/java/ui/settings/SelenideDriverSetup.java
+++ src/main/java/ui/settings/SelenideDriverSetup.java
@@ -1,19 +1,23 @@
 package ui.settings;
 
+import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeSuite;
 
 public class SelenideDriverSetup {
 
     private ChromeOptions options;
 
-    public ChromeOptions getOptions() {
+    @BeforeSuite
+    public void getOptions() {
         options = new ChromeOptions();
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("window-size=1280,768");
+        Configuration.browserCapabilities = new MutableCapabilities(options);
         WebDriverManager.chromedriver().setup();
-        return options;
     }
 }
