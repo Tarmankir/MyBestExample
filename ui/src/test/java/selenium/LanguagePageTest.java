6544b079814212fc8b64c0bcb20acb1d99f81dba
Tarmankir <bigkir@yandex.ru>
2022-05-03 13:30:53 +0300

    Refactoring selenium and selenide capabilities in UI and Mobile

--- ui/src/test/java/selenium/LanguagePageTest.java
+++ ui/src/test/java/selenium/LanguagePageTest.java
@@ -2,12 +2,12 @@ package selenium;
 
 import org.testng.annotations.Test;
 import selenium.pages.LanguagePage;
-import settings.SeleniumSelenoidSetup;
+import settings.SeleniumDriverSetup;
 
 import static org.openqa.selenium.support.PageFactory.initElements;
 import static settings.UiConfig.getUiSetting;
 
-public class LanguagePageTest extends SeleniumSelenoidSetup {
+public class LanguagePageTest extends SeleniumDriverSetup {
 
     @Test
     public void languagePageTest() {
