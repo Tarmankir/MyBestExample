9949c38bcb40957e67a94fd2cb871ee023b671f1
Tarmankir <bigkir@yandex.ru>
2023-04-24 19:32:13 +0300

    Add retryCount WebDriver in LanguagePageTest

--- ui/src/test/java/selenium/LanguagePageTest.java
+++ ui/src/test/java/selenium/LanguagePageTest.java
@@ -1,5 +1,6 @@
 package selenium;
 
+import org.openqa.selenium.WebDriverException;
 import org.testng.annotations.Test;
 import selenium.pages.LanguagePage;
 import settings.SeleniumDriverSetup;
@@ -9,9 +10,37 @@ import static settings.UiConfig.getUiSetting;
 
 public class LanguagePageTest extends SeleniumDriverSetup {
 
+    private final static int MAX_RETRY_COUNT = 5;
+
     @Test
     public void languagePageTest() {
-        driver.get(getUiSetting("mainURL"));
+        int retryCount = 0;
+        while(true)
+        {
+            try
+            {
+                driver.get(getUiSetting("mainURL"));
+                break;
+            }
+            catch(WebDriverException e)
+            {
+                if( retryCount > MAX_RETRY_COUNT )
+                {
+                    throw new RuntimeException("Too many retries...", e);
+                }
+
+                retryCount++;
+                try
+                {
+                    Thread.sleep(2_000);
+                }
+                catch (InterruptedException interruptedException)
+                {
+                    interruptedException.printStackTrace();
+                }
+            }
+        }
+//        driver.get(getUiSetting("mainURL"));
         LanguagePage languagePage = initElements(driver, LanguagePage.class);
         languagePage.clickLanguageSettings();
         languagePage.clickEngInterface();
